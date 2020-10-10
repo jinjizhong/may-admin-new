98a724879c7c394eaaf40834f07dad0f3a45bf62
jinjizhong <jinjizhong@futongsmart.com>
2020-10-10 17:34:23 +0800

    注释掉路由

--- src/router/index.js
+++ src/router/index.js
@@ -18,21 +18,6 @@ import Layout from '../views/layout/Layout'
     icon: 'svg-name'             the icon show in the sidebar,
   }
  **/
-// export const constantRouterMap = [
-//   {path: '/login', component: () => import('@/views/login/index'), hidden: true},
-//   {path: '/404', component: () => import('@/views/404'), hidden: true},
-//   {
-//     path: '',
-//     component: Layout,
-//     redirect: '/home',
-//     children: [{
-//       path: 'home',
-//       name: 'home',
-//       component: () => import('@/views/home/index'),
-//       meta: {title: '首页', icon: 'home'}
-//     }]
-//   }
-// ]
 
 export const constantRouterMap = [
   { path: '/login', component: () => import('@/views/login/index'), hidden: true },
